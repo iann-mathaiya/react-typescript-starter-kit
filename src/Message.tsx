526e7ad239e3c374616b575baa787e89f93a6c21
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-02 15:38:37 +0300

    tailwindcss added and correctly configured

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,9 +1,8 @@
 export default function Message() {
+  const name = "Ian";
 
-    const name = 'Ian'
+  if (name)
+    return <h1 className='text-3xl text-orange-500 font-bold'>Hello {name}</h1>;
 
-    if(name) return <h1>Hello {name}</h1>
-
-    return <h1>Hello World!</h1>
-    
+  return <h1 className='text-3xl font-bold'>Hello World!</h1>;
 }
