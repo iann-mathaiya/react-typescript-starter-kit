eba75ce1b5a1330b400b12524b11da6e21d311e5
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-02 16:18:16 +0300

    conditionally rendering lists update

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,21 @@
 import { Fragment } from "react"
 
 export default function ListGroup() {
-  const cities = ["New York", "London", "Nairobi", "Tokyo", "Sydney"]
+  let cities = ["New York", "London", "Nairobi", "Tokyo", "Sydney"]
 
   return (
     <Fragment>
-      <h1 className='text-2xl text-slate-800 font-semibold'>List</h1>
+      <h1 className='text-2xl text-slate-800 font-semibold'>Cities</h1>
+      {cities.length === 0 && (
+        <h2 className=' text-stone-500 font-medium'>No cities added yet.</h2>
+      )}
       <ul className='list-disc list-outside'>
         {cities.map((city) => (
-          <li key={city} className='text-slate-500 font-medium'>
+          <li
+            key={city}
+            onClick={(event) => console.log(`${city} visited`, event)}
+            className='text-stone-500 font-normal'
+          >
             {city}
           </li>
         ))}
