2ffa91e2003961ab7fea7e86d75b0ac8aaff15a0
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-02 16:50:06 +0300

    passing type safe data via component props

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import ListGroup from './components/ListGroup'
 import Message from './components/Message'
 
 export default function App() {
+  let cities = ["New York", "London", "Nairobi", "Tokyo", "Sydney"]
+  let countries = ["New Zealand", "Great Britain", "Kenya", "Japan", "Australia"]
+
   return (
-    <div className='max-w-7xl mx-auto p-4 sm:p-8'>
-      <ListGroup />
+    <div className='max-w-7xl mx-auto p-4 sm:p-8 space-y-8'>
+      <ListGroup list={cities} heading='Cities' />
+      <ListGroup list={countries} heading='Countries' />
     </div>
   )
 }
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,25 +1,30 @@
 import { Fragment, useState } from "react"
 
-export default function ListGroup() {
-  let cities = ["New York", "London", "Nairobi", "Tokyo", "Sydney"]
+interface ListGroupProps {
+  list: string[]
+  heading: string
+}
+
+export default function ListGroup({list, heading}: ListGroupProps) {
+  // let cities = ["New York", "London", "Nairobi", "Tokyo", "Sydney"]
   const [selectedIndex, setSelectedIndex] = useState(-1)
 
   const handleVisit = ( index: number ) => setSelectedIndex(index)
 
   return (
     <Fragment>
-      <h1 className='text-2xl text-slate-800 font-semibold'>Cities</h1>
-      {cities.length === 0 && (
+      <h1 className='text-2xl text-slate-800 font-semibold'>{heading}</h1>
+      {list.length === 0 && (
         <h2 className=' text-stone-500 font-medium'>No cities added yet.</h2>
       )}
       <ul className='list-disc list-outside'>
-        {cities.map((city, index) => (
+        {list.map((item, index) => (
           <li
-            key={city}
+            key={item}
             onClick={() => handleVisit(index)}
             className={selectedIndex === index ? 'text-stone-600 font-semibold cursor-default' : 'text-stone-500 font-normal cursor-pointer'}
           >
-            {city}
+            {item}
           </li>
         ))}
       </ul>
