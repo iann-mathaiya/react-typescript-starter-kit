65e3f69023e46956a09b069c58539ae0d1a03451
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-08 11:01:23 +0300

    using useState React hook to access input values

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,20 +1,14 @@
-import { FormEvent, useRef } from "react"
 import SubmitButton from "./SubmitButton"
+import { FormEvent, useState } from "react"
 
 export default function Form() {
+  const [menuItem, setMenuItem] = useState({ item: "", price: 0.00 })
 
-    const menuItemRef = useRef<HTMLInputElement>(null)
-    const priceRef = useRef<HTMLInputElement>(null)
+  const handleSumbit = (event: FormEvent) => {
+    event.preventDefault()
 
-    const menuItem = {item: '', price: 0}
-
-    const handleSumbit = (event: FormEvent) => {
-        event.preventDefault()
-        menuItem.item = menuItemRef.current!.value
-        menuItem.price = parseFloat(priceRef.current!.value)
-
-        console.log(menuItem)
-    }
+    console.log(menuItem)
+  }
 
   return (
     <form onSubmit={handleSumbit}>
@@ -28,9 +22,12 @@ export default function Form() {
         <div className='mt-2'>
           <input
             type='text'
-            ref={menuItemRef}
             id='menu-item-name'
+            value={menuItem.item}
             placeholder='Americano'
+            onChange={(event) =>
+              setMenuItem({ ...menuItem, item: event.target.value })
+            }
             className='block w-full rounded-md border-0 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-500 sm:text-sm sm:leading-6'
           />
         </div>
@@ -46,17 +43,22 @@ export default function Form() {
         <div className='mt-2'>
           <input
             id='price'
-            type='number'
-            ref={priceRef}
             step='.01'
+            type='number'
+            value={menuItem.price}
+            onChange={(event) =>
+              setMenuItem({
+                ...menuItem,
+                price: parseFloat(event.target.value),
+              })
+            }
             placeholder='Price'
             className='block w-full rounded-md border-0 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-500 sm:text-sm sm:leading-6'
           />
         </div>
       </div>
 
-      <SubmitButton backgroundColor="bg-sky-600">Submit</SubmitButton>
-
+      <SubmitButton backgroundColor='bg-sky-600'>Submit</SubmitButton>
     </form>
   )
 }
