a9e5c6ff79cfc07de505545d4f4925979fac5702
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-08 12:49:51 +0300

    using react hook forms to update the form component and validate form inputs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,36 +1,33 @@
+import { FieldValues, useForm } from "react-hook-form"
 import SubmitButton from "./SubmitButton"
 import { FormEvent, useState } from "react"
 
 export default function Form() {
-  const [menuItem, setMenuItem] = useState({ item: "", price: 0.00 })
+  const { register, handleSubmit, formState: {errors} } = useForm()
+  console.log(register("item"))
 
-  const handleSumbit = (event: FormEvent) => {
-    event.preventDefault()
-
-    console.log(menuItem)
-  }
+  const onSubmit = (data: FieldValues) => {}
 
   return (
-    <form onSubmit={handleSumbit}>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className='mb-3'>
         <label
           htmlFor='menu-item-name'
           className='block text-sm font-medium leading-6 text-gray-900'
         >
-          Menu Item
+          Menu Item Name
         </label>
         <div className='mt-2'>
           <input
             type='text'
             id='menu-item-name'
-            value={menuItem.item}
             placeholder='Americano'
-            onChange={(event) =>
-              setMenuItem({ ...menuItem, item: event.target.value })
-            }
+            {...register("item", { required: true, minLength: 4 })}
             className='block w-full rounded-md border-0 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-500 sm:text-sm sm:leading-6'
           />
         </div>
+       {errors.item?.type === 'required' && <p className="mt-2 text-red-500 text-sm font-normal">Menu Item Name is required </p>}
+       {errors.item?.type === 'minLength' && <p className="mt-2 text-red-500 text-sm font-normal">Menu Item Name should be at least 4 characters </p>}
       </div>
 
       <div className='mb-3'>
@@ -45,13 +42,7 @@ export default function Form() {
             id='price'
             step='.01'
             type='number'
-            value={menuItem.price}
-            onChange={(event) =>
-              setMenuItem({
-                ...menuItem,
-                price: parseFloat(event.target.value),
-              })
-            }
+            {...register("price")}
             placeholder='Price'
             className='block w-full rounded-md border-0 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-500 sm:text-sm sm:leading-6'
           />
