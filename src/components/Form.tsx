dab41d449fe53f92508900218285ac90118e75b5
Ian Mathaiya <iannmathaiya@gmail.com>
2023-11-08 13:31:45 +0300

    using zod to validate ther form inputs and provide necessary error messages

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,9 +1,25 @@
-import { FieldValues, useForm } from "react-hook-form"
+import { z } from "zod"
 import SubmitButton from "./SubmitButton"
-import { FormEvent, useState } from "react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { FieldValues, useForm } from "react-hook-form"
+
+const schema = z.object({
+  item: z
+    .string()
+    .min(4, { message: "Item name must be alleast 4 characters" }),
+  price: z
+    .number({ invalid_type_error: "Price is required" })
+    .min(1, { message: "Price must be atleast $1.00" }),
+})
+
+type FormData = z.infer<typeof schema>
 
 export default function Form() {
-  const { register, handleSubmit, formState: {errors} } = useForm()
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({ resolver: zodResolver(schema) })
   console.log(register("item"))
 
   const onSubmit = (data: FieldValues) => {}
@@ -26,8 +42,11 @@ export default function Form() {
             className='block w-full rounded-md border-0 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-500 sm:text-sm sm:leading-6'
           />
         </div>
-       {errors.item?.type === 'required' && <p className="mt-2 text-red-500 text-sm font-normal">Menu Item Name is required </p>}
-       {errors.item?.type === 'minLength' && <p className="mt-2 text-red-500 text-sm font-normal">Menu Item Name should be at least 4 characters </p>}
+        {errors.item && (
+          <p className='mt-2 text-red-500 text-sm font-normal'>
+            {errors.item.message}
+          </p>
+        )}
       </div>
 
       <div className='mb-3'>
@@ -42,10 +61,15 @@ export default function Form() {
             id='price'
             step='.01'
             type='number'
-            {...register("price")}
+            {...register("price", { valueAsNumber: true })}
             placeholder='Price'
             className='block w-full rounded-md border-0 py-1.5 px-2.5 text-gray-900 shadow-sm ring-1 ring-gray-300 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-sky-500 sm:text-sm sm:leading-6'
           />
+          {errors.price && (
+            <p className='mt-2 text-red-500 text-sm font-normal'>
+              {errors.price.message}
+            </p>
+          )}
         </div>
       </div>
 
